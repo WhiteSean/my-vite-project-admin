7be1b89484cfb220deb590e1e9dee56985e6e659
lixiaobing@mbse.com.cn <lixiaobing@mbse.com.cn>
2024-03-06 17:42:07 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import ArcoVue from '@arco-design/web-vue';
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
+import App from './App.vue';
+import router from './router';
+import '@arco-design/web-vue/dist/arco.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.use(ArcoVue);
+app.use(ArcoVueIcon);
+app.mount('#app');
